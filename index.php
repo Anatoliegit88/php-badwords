b41e595ff16c87edc4abdda9a0416d67c6797e41
Anatoliegit88 <anatolie.coroban88@gmail.com>
2022-11-24 16:36:31 +0100

    upgrade

--- index.php
+++ index.php
@@ -20,6 +20,15 @@
 
 <?php echo $Scrivi;?>
  
+<?php $pararafo = " lorem22 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis porro nobis et consequuntur tempore, itaque consectetur quae atque tenetur alias minus nemo sapiente aperiam tempora, aliquid repellendus. Temporibus, reprehenderit"; ?>
+ <p><?php echo $pararafo;?></p>
 
+
+
+ <h1>Cambio la parola ,,ipsum" con le "***"</h1>
+ <?php $nuovo_pararafo = str_replace("ipsum", "***", $pararafo);?>
+
+
+ <p><?php echo $nuovo_pararafo;?></p>
 </body>
 </html>
