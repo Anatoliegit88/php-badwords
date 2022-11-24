d7001ae0238e2706a31d095f1a75899201a22970
Anatoliegit88 <anatolie.coroban88@gmail.com>
2022-11-24 15:42:51 +0100

    upgrade

--- paragrafo.php
+++ paragrafo.php
@@ -1,10 +1,10 @@
 <?php $pararafo = " lorem22 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis porro nobis et consequuntur tempore, itaque consectetur quae atque tenetur alias minus nemo sapiente aperiam tempora, aliquid repellendus. Temporibus, reprehenderit"; ?>
- <p><?php echo $pararafo; ?></p>
+ 
 
 
 
- <h1>Cambio Parole con le "***"</h1>
- <?php $nuovo_pararafo = cambio_parola("ipsum", "***", $pararafo);?>
+ <h1>Cambio la parola ,,ipsum" con le "***"</h1>
+ <?php $nuovo_pararafo = str_replace("ipsum", "***", $pararafo);?>
 
 
  <p><?php echo $nuovo_pararafo;?></p>
